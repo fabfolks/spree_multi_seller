77156ea1bbce140c2ebd36ef33948291ff7d9f36
Sumanth Nagadavalli <sumansn@thoughtworks.com>
2013-07-29 09:43:22 +0530

    Adding a cancan ability for seller

---
+++ app/models/spree/seller_ability.rb
@@ -0,0 +1,19 @@
+class Spree::SellerAbility
+  include CanCan::Ability
+
+  def initialize(user)
+    self.clear_aliased_actions
+    alias_action :edit, to: :update
+    alias_action :new, to: :create
+    alias_action :new_action, to: :create
+    alias_action :show, to: :read
+    alias_action :delete, to: :destroy
+
+    user ||= Spree.user_class.new
+    if user.respond_to?(:has_spree_role?) && user.has_spree_role?('seller')
+      can :manage, Spree::Product do |product|
+        Spree::SellerUsers.find_all_by_user_id(user.id).collect(&:seller_id).include? product.seller_id
+      end
+    end
+  end
+end
--- lib/spree_multi_seller/engine.rb
+++ lib/spree_multi_seller/engine.rb
@@ -15,6 +15,7 @@ module SpreeMultiSeller
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+      Spree::Ability.register_ability(Spree::SellerAbility)
     end
 
     config.to_prepare &method(:activate).to_proc
