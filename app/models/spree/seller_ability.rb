d594eb30f153e8e9cff8e53c54505ce5b99a3253
Al Bobrov <al.bobrov@sv-lab.ru>
2013-11-14 19:43:03 +0600

    ability for products gush

--- app/models/spree/seller_ability.rb
+++ app/models/spree/seller_ability.rb
@@ -11,27 +11,61 @@ class Spree::SellerAbility
 
     user ||= Spree.user_class.new
     if user.respond_to?(:has_spree_role?) && user.has_spree_role?('seller')
-      #can :manage, :all
-      can :manage, Spree::SellerOrder do |order|
-        order.seller_id == user.seller.id
-      end
-      can :create, Spree::Product
-      can :manage, Spree::Product do |product|
-        product.seller_id == user.seller.id
-      end
-      can :manage, Spree::User do |spree_user|
-        !spree_user.seller.nil? and spree_user.seller.id == user.seller.id
-      end
+      
+      can :manage, :all
+
+        can :index, Spree::Order
+        can :show, Spree::Order
+        can :admin, Spree::Order
+
+        can :index, Spree::Product
+        can :show, Spree::Product
+        can :admin, Spree::Product
+
+        #can :index, Spree::Admin::Reports
+        can :manage, Spree::Report
+        can :show, Spree::Report
+        #can :admin, Spree::Admin::Reports
+
+        #can :manage, Spree::Variant
+        can :index, Spree::Variant
+        can :show, Spree::Variant
+        can :admin, Spree::Variant
+
+        can :manage, Spree::Adjustment
+
+        can :index, Spree::Taxon
+        can :index, Spree::Taxonomy
+        #manage
+
+      # can :manage, Spree::SellerOrder do |order|
+      #   order.seller_id == user.seller.last.id
+      # end
+      # can :create, Spree::Product
+      # can :manage, Spree::Product do |product|
+      #   product.seller_id == user.seller.id
+      # end
+      # can :manage, Spree::User do |spree_user|
+      #   !spree_user.seller.nil? and spree_user.seller.last.id == user.seller.last.id
+      # end
       can :manage, Spree::ProductProperty do |prodcut_property|
         product_property.product.seller_id == user.seller.id
       end
       can :manage, Spree::Variant do |variant|
         variant.product.seller_id == user.seller.id
       end
-      can :manage, Spree::Report
+      
       can :manage, Spree::OptionType
       can :manage, Spree::Property
       can :manage, Spree::Prototype
+        # base_admin
+        # custom_perms
+      
+        # %w/destroy/.each do |act|
+        #   can act, "admin/orders"
+        # end
+      
+        # can 'fire', 'cancel'
     end
   end
 end
--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -5,8 +5,8 @@ Spree::User.class_eval do
 
 	#scope :seller_user, Spree::User.includes(:spree_roles).where("spree_roles.name = 'seller'")
 	
-		def has_role?(role_in_question)
-	  	spree_roles.any? { |role| role.name == role_in_question.to_s }
-	  end
+	def has_role?(role_in_question)
+  	spree_roles.any? { |role| role.name == role_in_question.to_s }
+  end
 
 end
