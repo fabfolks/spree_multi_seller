72906e7223bf57911f186428be5c0359237bd7b7
Al Bobrov <al.bobrov@sv-lab.ru>
2013-11-15 10:36:44 +0600

    logo replace spree

--- app/overrides/admin.rb
+++ app/overrides/admin.rb
@@ -3,3 +3,9 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      :text => "<%= tab(:sellers) %>",
                      :disabled => false)
+#override logo
+Deface::Override.new(:virtual_path => "spree/admin/shared/_header",
+                     :name => "replace_logo",
+                     :replace=> "[data-hook='logo-wrapper']",
+                     :partial => "spree/admin/shared/logo",
+                     :disabled => false)
